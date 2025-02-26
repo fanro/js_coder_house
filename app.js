9ee77f1b2c46bb8bb692310fa3c6abb767f99499
mhruiz <maximiliano.hernan.ruiz@gmail.com>
2025-02-26 20:10:50 -0300

    productos to array

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let condition = true;
-let totalCarrito = 0;
 const IVA = 1.21;
 
 const calcularIva = (precio) => precio * IVA;
+
+const calcularTotal = (productos) => {
+  let total = 0;
+  for (let i = 0; i < productos.length; i++) {
+    total += productos[i].precio;
+  }
+  return total;
+};
+
+let productos = [];
 
 do {
   let eleccion =
@@ -10,31 +19,24 @@ do {
         1-Huevo de oro
         2-Huevo de plata
         3-Huevo de bronce
-        4-Ver total
-        5-Ver total con IVA
-        6-Salir de la tienda
+        4-Salir de la tienda
+        Total Carrito: $${calcularTotal(productos)}
+        Total IVA: $${calcularIva(calcularTotal(productos))}
+        Total Productos: ${productos.length}
         `);
 
   //console.log(eleccion);
   switch (eleccion) {
     case '1':
-      totalCarrito += 100000;
+      productos.push({ id: 1, nombre: 'Huevo de oro', precio: 100000 });
       break;
     case '2':
-      totalCarrito += 75000;
+      productos.push({ id: 2, nombre: 'Huevo de plata', precio: 75000 });
       break;
     case '3':
-      totalCarrito += 50000;
+      productos.push({ id: 3, nombre: 'Huevo de bronce', precio: 50000 });
       break;
     case '4':
-      alert(`El total hasta el momento es $${totalCarrito}`);
-      break;
-    case '5':
-      alert(
-        `El total hasta el momento con IVA es $${calcularIva(totalCarrito)}`
-      );
-      break;
-    case '6':
       condition = !confirm('Estas seguro de querer salir?');
       break;
     case null: // cancelar del prompt sin valor
