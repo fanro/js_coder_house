ea75fbb9ee0c77d3569dc39ad0e27c00c98e04b0
mhruiz <maximiliano.hernan.ruiz@gmail.com>
2025-03-19 19:50:43 -0300

    huevos con descuentos

--- app.js
+++ app.js
@@ -38,8 +38,21 @@ const getPrecioHuevo = (huevo) => {
   return formatCurrency(huevo.precio);
 };
 
+const getHuevosDescuento = (huevos, descuento) => {
+  return huevos.filter((huevo) => huevo.descuento == descuento);
+};
+
 class HuevoFaberge {
-  constructor(id, nombre, precio, imagen, composicion, dimensiones, peso) {
+  constructor(
+    id,
+    nombre,
+    precio,
+    imagen,
+    composicion,
+    dimensiones,
+    peso,
+    descuento
+  ) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
@@ -47,6 +60,7 @@ class HuevoFaberge {
     this.composicion = composicion;
     this.dimensiones = dimensiones;
     this.peso = peso;
+    this.descuento = descuento;
   }
 }
 
@@ -54,6 +68,7 @@ let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
 
 let productos = [];
+
 let huevo1 = new HuevoFaberge(
   1,
   'Huevo de oro',
@@ -64,8 +79,10 @@ let huevo1 = new HuevoFaberge(
     ['Plata 925', 25],
   ],
   '10x10x10',
-  '1kg'
+  '1kg',
+  0
 );
+
 let huevo2 = new HuevoFaberge(
   2,
   'Huevo de plata',
@@ -73,35 +90,42 @@ let huevo2 = new HuevoFaberge(
   'img/huevo-plata.jpg',
   [['Plata 925', 100]],
   '10x10x10',
-  '1kg'
+  '1kg',
+  5
 );
+
 let huevo3 = new HuevoFaberge(
   3,
   'Huevo de bronce',
   50000,
+  'img/huevo-bronce.jpg',
   [
     ['Bronce', 90],
     ['Plata 925', 10],
   ],
-  'img/huevo-bronce.jpg',
-  'Bronce',
   '10x10x10',
-  '1kg'
+  '1kg',
+  5
 );
+
 let huevo4 = new HuevoFaberge(
   4,
   'Huevo de cobre',
   25000,
+  'img/huevo-cobre.jpg',
   [
     ['Cobre', 80],
     ['Plata 925', 20],
   ],
-  'img/huevo-cobre.jpg',
-  'Cobre',
   '10x10x10',
-  '1kg'
+  '1kg',
+  20
 );
+
 productos.push(huevo1, huevo2, huevo3, huevo4);
+
+// let res = getHuevosDescuento(productos, 5);
+// console.log(res);
 
 do {
   let eleccion =
@@ -140,7 +164,6 @@ do {
           ? ` Tiene elementos en el carrito (${carrito.length})`
           : '';
       condition = !confirm(`Estas seguro de querer salir?${cantidad}`);
-      console.log(condition);
       if (!condition) {
         limpiarCarrito();
       }
